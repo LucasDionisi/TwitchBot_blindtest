4469ce8b01bbc625fd6198e5b41bd1ac73fa0f26
Lucas Dionisi <lucas.dionisi@gmail.com>
2023-02-01 21:45:27 +0100

    Possibility to hide table

--- src/js/script.js
+++ src/js/script.js
@@ -2,47 +2,59 @@ function run() {
     const scores = Scoreboard.getInstance().getScores();
     const songlist = Songlist.getInstance().getSonglist();
 
-    var tabScore = $('table#scores');
+    var tabScore = $('div#scores table');
     scores.forEach(score => {
         var elem = `<tr><td>${score.user}</td><td>${score.points}</td></tr>`;
         tabScore.append(elem);
     });
 
-    var tabSonglist = $('table#songlist');
-    var i = 1;
-    songlist.forEach(song => {
-        var elem = `<tr><td>${i}</td><td>${song.artist}</td><td>${song.title}</td><td>${song.points}</td><td><img class="icon" src="resources/icons/play.svg" alt="start song"></td><td><img class="icon" src="resources/icons/edit.svg" alt="start song"></td><td><img class="icon" src="resources/icons/delete.svg" alt="start song"></td></tr>`;
-        i++;
+    var tabSonglist = $('div#songlist table');
+    songlist.forEach((song, index )=> {
+        var elem = `
+        <tr>
+            <td>${index+1}</td>
+            <td>${song.artist}</td>
+            <td>${song.title}</td>
+            <td>${song.points}</td>
+            <td><img class="icon" src="resources/icons/play.svg" alt="start song"></td>
+            <td><img class="icon" src="resources/icons/edit.svg" alt="start song"></td>
+            <td><img class="icon" src="resources/icons/delete.svg" alt="start song">
+            </td>
+        </tr>
+        `;
         tabSonglist.append(elem);
     });
-
-
-    $("#addSongBtn").on('click', function () {
-        var fields = ["#artistInput", "#titleInput", "#pointsInput"];
-        var isValid = true;
-
-        for (var i = 0; i < fields.length; i++) {
-            var fieldValue = $(fields[i]).val();
-            if (fieldValue === "") {
-                $(fields[i]).css("border-color", "red");
-                isValid = false;
-            } else {
-                $(fields[i]).css("border-color", "");
-            }
-        }
-
-        if (!isValid) {
-            alert("Veuillez remplir tous les champs.");
-        } else {
-            console.log("Artiste: " + $("#artistInput").val());
-            console.log("Titre: " + $("#titleInput").val());
-            console.log("Points: " + $("#pointsInput").val());
-        }
-    });
-
-    $("input").on("input", function () {
-        if ($(this).val() !== "") {
-            $(this).css("border-color", "");
-        }
-    });
 }
+
+$('#addSongBtn').on('click', function () {
+    var fields = ['#artistInput', '#titleInput', '#pointsInput'];
+    var isValid = true;
+
+    for (var i = 0; i < fields.length; i++) {
+        var fieldValue = $(fields[i]).val();
+        if (fieldValue === '') {
+            $(fields[i]).css('border-color', 'red');
+            isValid = false;
+        } else {
+            $(fields[i]).css('border-color', '');
+        }
+    }
+
+    if (!isValid) {
+        alert('Veuillez remplir tous les champs.');
+    } else {
+        console.log('Artiste: ' + $('#artistInput').val());
+        console.log('Titre: ' + $('#titleInput').val());
+        console.log('Points: ' + $('#pointsInput').val());
+    }
+});
+
+$('input').on('input', function () {
+    if ($(this).val() !== '') {
+        $(this).css('border-color', '');
+    }
+});
+
+$('img#hide').on('click', function () {
+    $("#songlist table").toggleClass("blur");
+});
