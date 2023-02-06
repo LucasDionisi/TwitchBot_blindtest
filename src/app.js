43531840a1093c61bd620ecb04378227414d04c9
Lucas Dionisi <lucas.dionisi@gmail.com>
2023-02-06 17:55:24 +0100

    fix issues

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // INDEX HTML
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 const port = 8080;
